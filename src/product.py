536833e6ae44d5d49703ed864b88bb33f60fbecb
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-20 15:39:40 +0500

    products

--- src/product.py
+++ src/product.py
@@ -1,1 +1,13 @@
-a = 4
+class Product:
+    id = 10000000
+
+    def __init__(self, name: str, price: float, quantity: int):
+        self.product_id = Product.id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Product.id += 1
+
+    def info(self) -> str:
+        return f"{self.product_id}. {self.name}"
---
+++ tests.py
@@ -0,0 +1,9 @@
+from src.product import Product
+
+p1 = Product(name='Samsung s21', price=780.60, quantity=78)
+p2 = Product(name='Iphone 12', price=400.30, quantity=89)
+p3 = Product(name='Readme 14', price=450.00, quantity=63)
+
+print(p1.info())
+print(p2.info())
+print(p3.info())
